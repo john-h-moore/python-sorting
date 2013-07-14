76d55c508c5a2943068b7b756071585de1f2c33a
Grammaticus <grammaticus@grammaticus-linux.(none)>
2013-07-14 13:56:24 -0400

    corrected error in mergesort.py main (was calling nonexistant method)

--- mergesort.py
+++ mergesort.py
@@ -1,28 +1,28 @@
 def merge(left, right):
-	sorted = []
+	sortedlist = []
 	i, j = 0, 0
 	while i < len(left) and j < len(right):
 		if left[i] < right[j]:
-			sorted.append(left[i])
+			sortedlist.append(left[i])
 			i += 1
 		elif left[i] > right[j]:
-			sorted.append(right[j])
+			sortedlist.append(right[j])
 			j += 1
 		else:
-			sorted.append(left[i])
-			sorted.append(right[j])
+			sortedlist.append(left[i])
+			sortedlist.append(right[j])
 			i += 1
 			j += 1
 	if i < len(left):
-		sorted += left[i:]
+		sortedlist += left[i:]
 	elif j < len(right):
-		sorted += right[j:]
-	return sorted
+		sortedlist += right[j:]
+	return sortedlist
 
 def mergesort(unsorted):
 	if len(unsorted) < 3:
 		return merge(unsorted[:len(unsorted)/2], unsorted[len(unsorted)/2:])
 	else:
-		lsub = split(unsorted[:len(unsorted)/2])
-		rsub = split(unsorted[len(unsorted)/2:])
+		lsub = mergesort(unsorted[:len(unsorted)/2])
+		rsub = mergesort(unsorted[len(unsorted)/2:])
 	return merge(lsub, rsub)
